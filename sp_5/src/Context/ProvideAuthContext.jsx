1531e085ab413659c5cb96f48b0b2c2ee50eac80
Shakil <shakilpp123456@gmail.com>
2022-11-12 01:26:12 +0530

    day3 updation

--- sp_5/src/Context/ProvideAuthContext.jsx
+++ sp_5/src/Context/ProvideAuthContext.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import { AuthContext } from "./CreateAuthContext";
-console.log(AuthContext);
+// console.log(AuthContext);
 
 const initState = {
     isAuth : false,
---
+++ sp_5/src/RawMaterial/Home.js
@@ -0,0 +1,214 @@
+
+ const data = [
+    {
+      url:"https://akamaividz2.zee5.com/image/upload/w_899,h_506,c_scale,f_webp,q_auto:eco/resources/0-6-4z5230436/app_cover/1170x658withlogo4afcb2130773498a9a62ce9902183757.jpg",
+      title:"Bimbisarrr",
+      but:"",
+      buy:""
+    },
+    {
+      url:"https://akamaividz2.zee5.com/image/upload/w_899,h_506,c_scale,f_webp,q_auto:eco/resources/0-6-366/app_cover/1170x658withlogo917373f5b4fb416dbea586996f4fe8df.jpg",
+      title:"Bimbisa",
+      but:"",
+      buy:""
+    },
+    {
+      url:"https://akamaividz2.zee5.com/image/upload/w_899,h_506,c_scale,f_webp,q_auto:eco/resources/0-0-1z5250530/app_cover/1170x658withlogo8d01912bb073437b88492994c3631d76afa5840577c34b52b64393b0888d25a9.jpg",
+      title:"Bimbisar",
+      but:"",
+      buy:""
+    },
+    {
+      url:"https://akamaividz2.zee5.com/image/upload/w_899,h_506,c_scale,f_webp,q_auto:eco/resources/0-0-1z5227565/app_cover/1170x658withlogo28309bf839bf4a2da6221f530de1cf81.jpg",
+      title:"Bimbisarr",
+      but:"",
+      buy:""
+    },
+    {
+      url:"https://akamaividz2.zee5.com/image/upload/w_899,h_506,c_scale,f_webp,q_auto:eco/resources/0-6-4z5199975/app_cover/1170x658withlogo06e5d8ab3de440848da661c5f65d801d.jpg",
+      title:"Bimbisara",
+      but:"",
+      buy:""
+    },
+    {
+      url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5206274/portrait/1920x770485283887ce04c66a91f3e5dc0e20747.jpg",
+      title:"",
+      but:"",
+      buy:"",
+    }
+  ];
+  export default data;
+
+  const Trending = [
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5230436/portrait/1920x77080a9ccaeef524481ad49d14208180c8a.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5250530/portrait/1920x770129e19e854434fa98ff8734cfc2d6d32bf6bbf491bb046fdb38f88933efd4a78.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5199975/portrait/1920x770146d749f8ff14971b38653f6029302ad.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5194280/portrait/1920x77008b4524184ba4e3bbe0ba77829aeb4e6.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5227565/portrait/1920x7704ce388d5a6e74dac8ad9255657da588f.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5246147/portrait/1920x7703c6105c0047c4e98848e930ff18b4e38.jpg",
+      },
+  ];
+
+  const Top10_movies = [
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5246147/portrait/1920x7703c6105c0047c4e98848e930ff18b4e38.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5228525/portrait/1920x7701789cd01c98a4b098ff87b399b61530c3e52cc15ff174f4689cc1ebefe5b40ff.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5234688/portrait/1920x770b60b8550ec184b5eae16b5b5a3ae4579301be9f1145b4c46a2a8ea62adf5b37a.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5227557/portrait/1920x7708e66f35ad8484172a0796c13f18922c4.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5226617/portrait/1920x770c0fb8727e1894eb3aba1cf889861317c.jpg",
+      },
+      {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5206274/portrait/1920x770485283887ce04c66a91f3e5dc0e20747.jpg",
+      },
+  ]
+  const Latest_Hindi_ep = [
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_264,h_149,c_scale,f_webp,q_auto:eco/resources/0-1-6z5247937/list/00000263afa179b040b8456f956682237137a56d.jpg",
+        title:"Kundali Bhagya",
+        episode:"E1373  -  11 Nov"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_264,h_149,c_scale,f_webp,q_auto:eco/resources/0-1-6z5247724/list/0000017334d73a3bb29a4d589e02c50cad9e65d4.jpg",
+        title:"Bhagya Lakshmi",
+        episode:"E394  -  11 Nov"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_264,h_149,c_scale,f_webp,q_auto:eco/resources/0-1-6z5247730/list/000000657efd01bd48d444a5a10a86b6a619ef43.jpg",
+        title:"Kumkum Bhagya",
+        episode:"E2261  -  11 Nov"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_264,h_149,c_scale,f_webp,q_auto:eco/resources/0-1-6z5247893/list/00000267998c7848361f41e1ac983b33a2630ed0.jpg",
+        title:"Main Hoon Aparajita",
+        episode:"E44  -  11 Nov"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_264,h_149,c_scale,f_webp,q_auto:eco/resources/0-1-6z5247774/list/0000020338108771221a46f5bb1c1f12d887b3e0.jpg",
+        title:"Pyar Ka Pehla Naam: Radha Moh...",
+        episode:""
+    },
+
+  ]
+
+  const Top10_webseries = [
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5230436/portrait/1920x77080a9ccaeef524481ad49d14208180c8a.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5173774/portrait/1920x7706e8d8d70922c434c94dd09c14ccc5d5b.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5193948/portrait/1920x7704ba850de7a8a410cb16fa2d7468d3e77.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5192239/portrait/1920x770a78be5fdc4ef469fa04a1dbeaa702c2e.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-2176/portrait/1920x770b9b395ce22814c99b8217672c0e7deb7.jpghttps://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-2176/portrait/1920x770b9b395ce22814c99b8217672c0e7deb7.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-6-4z5145215/portrait/1920x770abf42130d9cf41dab2bf9e648cf3193b4100d873cc6547a8a4a4c92739160b3f.jpg"
+    },
+  ]
+
+  const Blockbuster_films = [
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-33204/portrait/1920x770eb95d00347474d48a4307809cedc50a0.jpg",
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5133458/portrait/1920x7700b75e1f80c204a0b82dfad6f59fd7f8ce7171601b0534b17aa8968531f3bc8f1.jpg",
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5133622/portrait/1920x7705ef1215cc2984b548b3356566bf54c3d9e3d4a78407d4579938e19522c963b86.jpg",
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-369273/portrait/1920x7706dbcd377e0ba4e40b8b4533066e64b791b570aed8b7047a6b8593ffac10e0ce9.jpg",
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z595840/portrait/1920x7705d9c7a86295d4f268cd4c7deb387d9b8.jpg",
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-101-10z5226524/portrait/1920x770e578d29207064883898fd35bbf51977d.jpg",
+    },
+  ]
+
+  const premiun_Tv_ep = [
+    
+        {
+            url:"https://akamaividz2.zee5.com/image/upload/w_264,h_149,c_scale,f_webp,q_auto:eco/resources/0-1-6z5247724/list/0000017334d73a3bb29a4d589e02c50cad9e65d4.jpg",
+            title:"Bhagya Lakshmi",
+            episode:"E394  -  11 Nov"
+        },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_264,h_149,c_scale,f_webp,q_auto:eco/resources/0-1-6z5247821/list/000002541ad4444de36e40a39f2197358a18c9d5.jpg",
+        title:"Meet",
+        episode:"E409  -  11 Nov"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_264,h_149,c_scale,f_webp,q_auto:eco/resources/0-1-6z5247964/list/000002256a5a04a4b09e4b3b956985ef02d40653.jpg",
+        title:"Pyaar tune kya kiya",
+        episode:"E1373  -  11 Nov"
+    },
+    
+  ]
+
+  const PopularMovies = [
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5191648/portrait/1920x77015755573326f485eb9b2ad856c296abd.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5149852/portrait/1920x770329e7efce50a434cb76540abee106a8c.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5227565/portrait/1920x7704ce388d5a6e74dac8ad9255657da588f.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-101-10z5145273/portrait/1920x770575c84286cdf4b8697c5c6007d0b7e9d26d6a675c3d2437d8e85cead413d13b2.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5177236/portrait/1920x77038551f46e9924100b57bd36b1739fe87.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5228525/portrait/1920x7701789cd01c98a4b098ff87b399b61530c3e52cc15ff174f4689cc1ebefe5b40ff.jpg"
+    },
+  ]
+  const World_hits = [
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5246201/portrait/1920x7702dea4d55e70f414f9e097f1e2d640628.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5243021/portrait/1920x770a9d390ff517b482d9e051723d481ba1c.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5233808/portrait/1920x770e1e200eb2ab84c2bb678ee60e3dcf06b243111bceaad490a94d223a2872f5a7a.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5233987/portrait/1920x770f349f780a7ec4af788748c6950b542ed.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5233982/portrait/1920x770c0b78d049dbb48a6b728580dec399d80.jpg"
+    },
+    {
+        url:"https://akamaividz2.zee5.com/image/upload/w_231,h_347,c_scale,f_webp,q_auto:eco/resources/0-0-1z5233652/portrait/1920x770908959635565404fb8b0681177cb75ab.jpg"
+    },
+  ]
+  export {Trending, Top10_movies, Latest_Hindi_ep, Top10_webseries, Blockbuster_films, premiun_Tv_ep, PopularMovies, World_hits} 
