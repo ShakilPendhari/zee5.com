47a8b2572db21bfc1e2b42dfef29e738b3f53183
Shakil <shakilpp123456@gmail.com>
2023-07-24 11:52:54 +0530

    login functinality ready

--- sp_5/src/Components/Login.jsx
+++ sp_5/src/Components/Login.jsx
@@ -3,6 +3,11 @@ import style from "./../style/ComponentElement/login.module.css";
 import codes from "country-calling-code";
 
 import { Box, Flex, Heading, Image, Input, Text } from "@chakra-ui/react";
+import { ChevronUpIcon } from "@chakra-ui/icons";
+
+
+let loginButtonStyleAllow = {cursor:"pointer",opacity:"1",fontWeight:"700",letterSpacing:"0.04rem",color:"#FFFFFF"}
+let loginButtonStyleDontAllow = {cursor:"not-allowed",opacity:"0.5",fontWeight:"700",letterSpacing:"0.04rem",color:"#FFFFFF80"}
 
 let obj = { data: "" };
 
@@ -13,61 +18,90 @@ const Login = () => {
   const [isNumber, setIsNumber] = useState(false);
   const [dropDown, setDropDown] = useState(false);
   const [isShowMobileError, setIsShowMobileError] = useState(false);
+  const [isbtndisabled, setisbtndisabled] = useState(true);
 
   const handleonChange = (e) => {
     const { name, value } = e.target;
-   
+  
     setIntvalue((val) => {
-
       let newVal = val.data + value;
 
-      if ((newVal * 1) / 1 === newVal * 1) {
+      // check isNumber or not
+      if (+value>=0) {
         setIsNumber(() => true);;
-        setDropDown(()=>true);
       } else {
         setIsNumber(() => false);
-        setDropDown(()=>false);
       }
-     console.log("value:",value,+value>=0)
 
-      if(value&&+value>=0)
-      {
-        setDropDown(()=>true)
-        if(value.length<10 || value.length>10)
+     
+        if(value&&+value>=0)
         {
-           setIsShowMobileError(()=>true);
-           setFlag(() => false);
+          setFlag(() => false);
+          if(value.length<10 || value.length>10)
+          {
+             setIsShowMobileError(()=>true);
+             setisbtndisabled(()=>true);
+             
+          }
+          else{
+             setIsShowMobileError(()=>false);
+             setIsNumber(()=>true);
+             setisbtndisabled(()=>false);
+          }
         }
         else{
-           setIsShowMobileError(()=>false);
-           setIsNumber(()=>true);
-           setFlag(() => false);
-        }
-      }
-      else{
-        setDropDown(()=>false);
-        setIsNumber(() => false);
-        if (newVal.length === 1 && Intvalue.data.length === 1 ) {
-          setFlag(() => true);
+          setIsNumber(() => false);
           setIsShowMobileError(()=>false);
-         
-        } else {
-          setFlag(() => false);
-          setIsShowMobileError(()=>false);
+          if (newVal.length === 1 && Intvalue.data.length === 1 ) {
+            setFlag(() => true);
+            setisbtndisabled(()=>true)
+           
+          } else {
+            setFlag(() => false);
+              let checkAtTheRateSign = value.trim().split("@");
+              if(checkAtTheRateSign.length===2)
+              {
+                let g = checkAtTheRateSign[1];
+                console.log("gg:",g)
+                g = g.trim().split(".");
+                console.log("gg:",g)
+                if((g[0]&&g[0].length>=1)&&(g[1]&&(g[1].length>=2&&g[1].length<=4)))
+                {
+                  console.log(value)
+                  if(value.length>1)
+                  {
+                    setisbtndisabled(()=>false)
+                  }
+                }
+                else{
+                  setisbtndisabled(()=>true)
+                }
+              }
+              else{
+                setisbtndisabled(()=>true)
+              }
+          }
         }
-      }
-     
+
       return { ...val, [name]: value };
     });
   };
 
-  return (
-    <Box className={style.login}>
-      {/* {
-     
-      codes.forEach((el)=>{console.log(el.countryCodes[0])})
-    } */}
+  const handleDropdown = ()=>{
+       setDropDown((menu)=>!menu);
+  }
 
+  const handleMouseLeave = ()=>{
+      setDropDown(()=>false)
+  }
+
+  const handleSubmit = ()=>{
+    console.log("hello this is input value",Intvalue)
+  }
+
+ 
+  return (
+    <Box className={style.login} onMouseLeave={handleMouseLeave}>
       <Heading
         fontSize={"1.8rem"}
         p="0rem 0rem 0.5rem"
@@ -78,7 +112,7 @@ const Login = () => {
         Login to ZEE5
       </Heading>
       <Text
-        w="90%"
+        w="82%"
         fontWeight="500"
         textAlign="center"
         className={style.subhead}
@@ -106,9 +140,13 @@ const Login = () => {
         outline="none"
         border={flag ? "1px solid red" : "1px solid black"}
         borderRadius="5px"
+        alignItems="center"
         className={style.input}
       >
-        <Text display={isNumber?"block":"none"} p="0.5rem">{ phoneCode }</Text>
+        <Flex pos="relative" onClick={handleDropdown} display={isNumber?"flex":"none"} justifyContent="center" alignItems="center">
+          <Text p="0.5rem">{ phoneCode }</Text>
+          <ChevronUpIcon width="2rem" height="2rem" transition="transform 0.5s ease-in 0.1s" transform={(dropDown)?"rotate(0deg)":"rotate(180deg)"}  />
+        </Flex>
         <Input
           value={Intvalue.data}
           onChange={handleonChange}
@@ -127,49 +165,60 @@ const Login = () => {
           zIndex="100"
           className="phoneNumber"
           bgColor="white"
-          borderRadius={"5px"}
+          borderRadius={"3px"}
           pos="absolute"
           top="3.8rem"
           left="-0.1rem"
         >
           {codes.map((el, i) => (
-            <Text onClick={(e)=>setPhoneCode(()=>e.target.textContent)} className={style.options} key={i}>
+            <Text onClick={(e)=>{setPhoneCode(()=>e.target.textContent); setDropDown(()=>false)}} className={style.options} key={i}>
               {el.countryCodes[0]}
             </Text>
           ))}
         </Box>
       )}
-      </Flex>
+
+
+      {/* Error section  */}
+
       {
-         isShowMobileError&&<Text
-        w="80%"
-        m="auto"
-        textAlign={"left"}
-        color="red"
-        fontSize={"0.75rem"}
+        isShowMobileError&&<Text
+         pos="absolute"
+         bottom="-1.17rem"
+         w="80%"
+         m="auto"
+         textAlign={"left"}
+         color="red"
+         fontSize={"0.70rem"}
+         letterSpacing="0.02rem"
+         fontWeight="500"
       >
         Incorret Mobile Number
       </Text>
       }
       <Text
         visibility={flag ? "visible" : "hidden"}
-        w="80%"
-        m="auto"
-        textAlign={"left"}
-        color="red"
-        fontSize={"0.75rem"}
+         pos="absolute"
+         bottom="-1.17rem"
+         w="80%"
+         m="auto"
+         textAlign={"left"}
+         color="red"
+         fontSize={"0.70rem"}
+         letterSpacing="0.02rem"
+         fontWeight="500"
       >
         Enter Email ID OR Mobile Number
       </Text>
-      
+      </Flex>
       <Box>
-        <button disabled="disabled" className={style.button}>
+        <button style={isbtndisabled?loginButtonStyleDontAllow:loginButtonStyleAllow} onClick={handleSubmit} disabled={isbtndisabled} className={style.button}>
           Login
         </button>
       </Box>
 
-      <Text>
-        New to ZEE5 ? <span>Register</span>
+      <Text cursor="pointer">
+       <span style={{color:"#000000"}} >New to ZEE5 ?</span>  <span id={style.register}>Register</span>
       </Text>
     </Box>
   );
