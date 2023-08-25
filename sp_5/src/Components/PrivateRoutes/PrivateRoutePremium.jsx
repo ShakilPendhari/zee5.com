5c2e9509ce33c75acdf0bd835947a905ea1b2452
Shakil <shakilpp123456@gmail.com>
2023-08-25 22:13:01 +0530

    premium section just stopped for some time

--- sp_5/src/Components/PrivateRoutes/PrivateRoutePremium.jsx
+++ sp_5/src/Components/PrivateRoutes/PrivateRoutePremium.jsx
@@ -1,12 +1,16 @@
 import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
 const PrivateRoutePremium = ({ children }) => {
-  const { isPremium } = useSelector((store) => store.data);
+  // const { isPremium } = useSelector((store) => store.data);
 
   // console.log("isPremium:", isPremium);
 
-  if (isPremium) {
+  // if (isPremium) {
+  //   return <Navigate to="/payment" />;
+  // }
+
+  if (false) {
     return <Navigate to="/payment" />;
   }
 
