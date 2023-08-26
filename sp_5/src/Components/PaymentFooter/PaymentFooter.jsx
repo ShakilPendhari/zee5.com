6d10e448c86e1cfe44acb74ba06fc7bb7756cf0b
Shakil <shakilpp123456@gmail.com>
2023-08-26 23:33:34 +0530

    now onwards navbar is sticky

--- sp_5/src/Components/PaymentFooter/PaymentFooter.jsx
+++ sp_5/src/Components/PaymentFooter/PaymentFooter.jsx
@@ -5,16 +5,17 @@ const PaymentFooter = () => {
   return (
     <Flex
     //   border="1px solid green"
+    borderTop="1px solid rgba(255, 255, 255, 0.05);"
       flexDir={{ base: "column-reverse", md: "row" }}
       height={{base:"5rem",md:"2rem"}}
       alignItems={{ base: "center", md: "flex-end" }}
       justifyContent={{ base: "space-evenly", md: "space-between" }}
       mb={{base:"0rem",md:"1rem"}}
     >
-      <Box fontSize="0.9rem" color="grey">
+      <Box fontSize={{base:"0.65rem",md:"0.75rem"}} color="grey">
         Copyright © 2023 Zee Entertainment Enterprises Ltd. All rights reserved.
       </Box>
-      <Flex gap="1rem" fontSize="0.9rem" color="grey">
+      <Flex gap="1rem" fontSize={{base:"0.75rem",md:"0.85rem"}} color="grey">
         <Text>About Us</Text>
         <Text>|</Text>
         <Text>Help Center</Text>
